a06618e442f8fbf10bd0817f7ca351b689861e42
totorowg <wangchingying@gmail.com>
2018-01-31 15:06:35 +0800

    開相機拍照存到手機裡再讀出來到imageView

--- app/src/main/java/com/cyw/a2018013101/MainActivity.java
+++ app/src/main/java/com/cyw/a2018013101/MainActivity.java
@@ -2,11 +2,21 @@ package com.cyw.a2018013101;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     ImageView img;
@@ -16,11 +26,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         img = findViewById(R.id.imageView);
     }
+    //把照相機打開照相並存到旁邊小圖示
     public void click1(View v)
     {
         Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(it, 123);
     }
+
+    //把圖片存到指定的手機資料夾(真實的手機應該會自動存到照片資料夾或download),並放小圖示
+    public void click2(View v)
+    {
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File f = new File(getExternalFilesDir("PHOTO"), "myphoto.jpg");
+        it.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        startActivityForResult(it, 456);
+    }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -34,6 +56,54 @@ public class MainActivity extends AppCompatActivity {
                 //會把照到的"縮圖"存在image view裡
                 img.setImageBitmap(bmp);
             }
+
         }
+        if (requestCode == 456)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                File f = new File(getExternalFilesDir("PHOTO"), "myphoto.jpg");
+                try {
+                    InputStream is = new FileInputStream(f);
+                    Log.d("BMP", "Can READ:" + is.available());
+                    Bitmap bmp = getFitImage(is);
+                    img.setImageBitmap(bmp);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+    public static Bitmap getFitImage(InputStream is)
+    {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = false;
+        byte[] bytes = new byte[0];
+        try {
+            bytes = readStream(is);
+            //BitmapFactory.decodeStream(inputStream, null, options);
+            Log.d("BMP", "byte length:" + bytes.length);
+            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+            System.gc();
+            // Log.d("BMP", "Size:" + bmp.getByteCount());
+            return bmp;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static byte[] readStream(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while ((len = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, len);
+        }
+        outStream.close();
+        inStream.close();
+        return outStream.toByteArray();
     }
 }
